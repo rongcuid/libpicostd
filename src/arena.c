59fb4b3ffa8ff815023eb7be5a8b9eea6616688e
Rongcui Dong <rongcuid@outlook.com>
2026-02-11 15:24:11 +0000

    Fix a small typo

--- src/arena.c
+++ src/arena.c
@@ -5,14 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-arena_t arena_from_buf(void *buf, size_t capacity) {
+arena_t arena_from_buf(void *buf, size_t capacity)
+{
   arena_t a = {0};
   a.begin = buf;
   a.end = a.begin + capacity;
   return a;
 }
 
-arena_t arena_suballoc(arena_t *a, size_t capacity) {
+arena_t arena_suballoc(arena_t *a, size_t capacity)
+{
   arena_t b = {0};
   return arena_from_buf(arena_malloc(a, capacity, 1), capacity);
 }
@@ -23,11 +25,13 @@ arena_t arena_suballoc(arena_t *a, size_t capacity) {
     * https://nullprogram.com/blog/2023/12/17/
     Modified.
  */
-void *arena_malloc(arena_t *a, size_t size, size_t align) {
+void *arena_malloc(arena_t *a, size_t size, size_t align)
+{
   // This arena allocator allocates from the end.
   ptrdiff_t available = a->end - a->begin;
   ptrdiff_t padding = -(ptrdiff_t)size & (align - 1);
-  if (size > available - padding) {
+  if (size > available - padding)
+  {
     return NULL;
   }
   return a->end -= size + padding;
@@ -39,9 +43,11 @@ void *arena_malloc(arena_t *a, size_t size, size_t align) {
     * https://nullprogram.com/blog/2023/12/17/
     Modified.
  */
-void arena_free(arena_t *a, void *ptr, size_t size, size_t align) {
+void arena_free(arena_t *a, void *ptr, size_t size, size_t align)
+{
   // Only the last object reclaims memory when freed.
-  if (ptr == a->end) {
+  if (ptr == a->end)
+  {
     ptrdiff_t padding = -size & (align - 1);
     a->end += size + padding;
   }
@@ -54,9 +60,10 @@ void arena_free(arena_t *a, void *ptr, size_t size, size_t align) {
     Modified.
  */
 void *arena_realloc(arena_t *a, void *ptr, size_t old_size, size_t new_size,
-                    size_t align) {
+                    size_t align)
+{
   // This just allocates a new region without reclaiming memory
-  assert(new >= old);
+  assert(new_size >= old_size);
   void *r = arena_malloc(a, new_size, align);
   return memcpy(r, ptr, old_size);
 }
